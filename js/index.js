c1c7bc515d6698a90fa423afa4c273e68982053d
Vinay Puppal <me@vinaypuppal.com>
2017-01-17 20:59:47 +0530

    basic-landing-page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const menuToggle = () => {
+  const menuBtn = document.querySelector('.js-menu')
+  const menu = document.querySelector('nav ul')
+  menuBtn.addEventListener('click', () => {
+    console.log('working')
+    menuBtn.classList.toggle('open')
+    menu.classList.toggle('open')
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  menuToggle()
+})
